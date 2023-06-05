22c5d299654fdbb510e477cac71a0b18664f18a9
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-06-04 21:58:37 -0400

    Add app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const searchButton = document.querySelector("#seach-btn");
+const menu = document.querySelector("#menu-buttons");
+const inputSearch = document.createElement("input");
+inputSearch.setAttribute("type", "search");
+inputSearch.classList.add("animate-AnimSearch", "w-72", "relative", "p-2", "rounded-md", "text-black", "max-[700px]:hidden");
+
+searchButton.addEventListener("click", () => {
+    menu.appendChild(inputSearch);
+    searchButton.before(inputSearch);
+    inputSearch.classList.toggle("hidden");
+    inputSearch.select();
+});

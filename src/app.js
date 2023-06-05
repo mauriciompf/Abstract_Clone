536dd636ca7b7750d6631d2bb983b091fb57078c
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-06-04 22:37:15 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ inputSearch.setAttribute("type", "search");
 inputSearch.classList.add("animate-AnimSearch", "w-72", "relative", "p-2", "rounded-md", "text-black", "max-[700px]:hidden");
 
 searchButton.addEventListener("click", () => {
-    menu.appendChild(inputSearch);
-    searchButton.before(inputSearch);
-    inputSearch.classList.toggle("hidden");
-    inputSearch.select();
+	menu.appendChild(inputSearch);
+	searchButton.before(inputSearch);
+	inputSearch.classList.toggle("hidden");
+	inputSearch.select();
 });
